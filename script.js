59b24cc1a70995606c848f4fc1ccb28be0873cc4
xrsQugi <02.04.5.art@gmail.com>
2023-06-23 16:29:10 +0300

    animation

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) =>{
+		console.log('entry :>> ', entry);
+		if(entry.isIntersecting){
+			entry.target.classList.add('show');
+		} else{
+			entry.target.classList.remove('show');
+		}
+	})
+})
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => {observer.observe(el)});
